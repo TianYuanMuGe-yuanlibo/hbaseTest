cf3a1310c91f240d04ad98517caa37eb7f7a2992
yuanlibo <936576946@qq.com>
2021-05-14 11:01:45 +0800

    冲突合并 master提交

--- src/main/java/com/huawei/apis/TestGit.java
+++ src/main/java/com/huawei/apis/TestGit.java
@@ -3,6 +3,6 @@ package com.huawei.apis;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("hello Git2");
+        System.out.println("hello Git conflict");
     }
 }
